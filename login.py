e465d2afd14cd9e3a4a6d0fab5a98f711dbb0014
boogerman919 <raytsai919@gmail.com>
2020-04-13 18:02:18 +0800

    decriptions added to the code

--- login.py
+++ login.py
@@ -21,6 +21,7 @@ SECRET_KEY = 'nf8Z3vuN2KFIVzgvAa3odLVZcK0dklQy'
 OCR_URL = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
 TOKEN_URL = 'https://aip.baidubce.com/oauth/2.0/token'
 
+# 拿API token
 def fetch_token():
     params = {'grant_type': 'client_credentials',
               'client_id': API_KEY,
@@ -55,21 +56,24 @@ def _request(url, data):
     except URLError as err:
         print(err)
 
+# 檢查登入是否成功
 def login_success():
+    # 確認是否有登入錯誤的關閉按鈕
     try:
         browser.find_element_by_xpath("//div[@class='ui-dialog-buttonset']/button[1]").click()
     except NoSuchElementException:
         return True
     return False
 
+# 獲取成績
 def get_grades():
-    ActionChains(browser).move_to_element(browser.find_element_by_xpath("//li[@name='01各項查詢']/a")).perform()
-    browser.find_element_by_name('A0410S').click()
+    ActionChains(browser).move_to_element(browser.find_element_by_xpath("//li[@name='01各項查詢']/a")).perform()  # 進入各項查詢項目
+    browser.find_element_by_name('A0410S').click()  # 點擊查詢個人成績
     sleep(0.5)
     browser.find_element_by_id("contents").click()
     semesters = browser.find_elements_by_xpath("//tr[@class='ui-widget-content jqgrow ui-row-ltr']")
     year = []
-    for row in semesters:
+    for row in semesters:  # 確認學期數
         year.append(row.find_element_by_css_selector("td:nth-child(2)").text)
     if len(year) <= 2:
         semesters = semesters[0]
@@ -82,13 +86,13 @@ def get_grades():
                 break
     year = len(year) - 2
     done = False
-    for sem in semesters:
+    for sem in semesters:  # 迭代學期
         sem.click()
         sleep(1)
         tests = list(browser.find_elements_by_xpath("//tr[@class='ui-widget-content jqgrow ui-row-ltr']"))
         rows = len(tests) - 2
         tests = tests[year:year+3]
-        for test in tests:
+        for test in tests:  # 迭代考試
             try:
                 test.click()
             except ElementClickInterceptedException:
@@ -101,7 +105,7 @@ def get_grades():
                 if s.find_element_by_css_selector("td:nth-child(5)").text[0] not in "0123456789":
                     scores[s.find_element_by_css_selector("td:nth-child(5)").text] = s.find_element_by_css_selector(
                         "td:nth-child(6)").text
-            print(scores)
+            print(scores)  # 印出成績
 loggedin = False
 login_tries = 0
 token = fetch_token()
@@ -112,19 +116,19 @@ options.add_argument('--no-sandbox')
 #options.add_argument("headless")
 browser = webdriver.Chrome(chrome_options=options)
 browser.get(url)
-while not loggedin:
+while not loggedin:  # 循環登入程序直到成功
     validate_result = ""
     validate_img = browser.find_element_by_id('validatePic').get_attribute("src")[22:].encode(encoding="utf-8")
     result_json = json.loads(_request(image_url, urlencode({'image': validate_img})))
     for words_result in result_json["words_result"]:
         validate_result += words_result["words"]
     validate_result = validate_result.strip()
-    browser.find_element_by_id('loginId').send_keys(Account)
-    browser.find_element_by_id('pas1').send_keys(Password)
-    browser.find_element_by_id('validateCode').send_keys(validate_result)
-    browser.find_element_by_id('login').click()
+    browser.find_element_by_id('loginId').send_keys(Account)    # 輸入帳號
+    browser.find_element_by_id('pas1').send_keys(Password)  # 輸入密碼
+    browser.find_element_by_id('validateCode').send_keys(validate_result)   # 輸入驗證碼
+    browser.find_element_by_id('login').click() # 點擊登入鍵
     time.sleep(0.1)
-    loggedin = login_success()
+    loggedin = login_success()  # 確認是否登入成功
 browser.implicitly_wait(10)
 
-get_grades()
+get_grades()   # 獲取成績
