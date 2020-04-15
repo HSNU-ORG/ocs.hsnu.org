523826eb453937e32e72d8e309cd373c598abac9
Anmicius <allenlin@disroot.org>
2020-04-15 21:09:38 +0800

    main.py: remove unused variable

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from ocs.login import login, get_grades
 url = "https://sschool.tp.edu.tw/Login.action?schNo=330301"
-Account = ""
-Password = ""
 
 if __name__ == '__main__':
     Account = input('enter account: ')
     Password = input('enter password: ')
+    print(Account, Password)
     login(url, Account, Password)
     get_grades()
