2e64e3d1208fee4dae07189546875b526f8ab0f3
boogerman919 <raytsai919@gmail.com>
2020-04-20 13:05:27 +0800

    yeet

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from ocs.login import login, get_grades
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+import os
+
+load_dotenv()
+
 url = "https://sschool.tp.edu.tw/Login.action?schNo=330301"
-Account = ''
-Password = ''
+Account = os.getenv("ACCOUNT")
+Password = os.getenv("PASSWORD")
 
 def main(request):
     get_grades(url, Account, Password)
+
+main(1)
